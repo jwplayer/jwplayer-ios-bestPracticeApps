c0e1cab85533fc7c1ac37fb0da8f9f61a0681adf
Mike Salvia <msalvia@jwplayer.com>
2020-09-24 15:30:37 -0400

    Small tweaks

--- JWBestPracticeApps/buildAll.sh
+++ JWBestPracticeApps/buildAll.sh
@@ -4,13 +4,13 @@ set +x
 allTargets=(Casting NativeControls JWAirPlay Voicer JWConviva JWCasting BasicVideoViewController FeedCollectionViewController FeedTableViewController AutoplayVideoFeed GoogleDAI JWFairPlayDrm)
 for t in ${allTargets[@]}; do
   echo "Building $t"
-  set -o pipefail && xcodebuild -derivedDataPath $(pwd)/DerivedData -workspace JWBestPracticeApps.xcworkspace -scheme $t clean archive -configuration Release -sdk iphoneos -archivePath $(pwd)/$t/build/$t.xcarchive OBJROOT=$(pwd)/$t/build SYMROOT=$(pwd)/$t/build PODS_CONFIGURATION_BUILD_DIR=$(pwd)/$t/build | xcpretty
+  set -o pipefail && xcodebuild -derivedDataPath $(pwd)/DerivedData -workspace JWBestPracticeApps.xcworkspace -scheme $t clean archive -configuration Release -archivePath $(pwd)/$t/build/$t.xcarchive -sdk iphoneos | xcpretty
   set -o pipefail && xcodebuild -allowProvisioningUpdates -exportArchive -archivePath $(pwd)/$t/build/$t.xcarchive -exportOptionsPlist exportOptions.plist -exportPath $(pwd)/$t/build/Release-iphoneos | xcpretty
 done
 
 allWatchOsTargets=(JWRemoteCastPlayer JWRemotePlayer)
 for t in ${allWatchOsTargets[@]}; do
   echo "Building $t"
-  set -o pipefail && xcodebuild -derivedDataPath $(pwd)/DerivedData -workspace JWBestPracticeApps.xcworkspace -scheme $t clean archive -configuration Release -archivePath $(pwd)/$t/build/$t.xcarchive OBJROOT=$(pwd)/$t/build SYMROOT=$(pwd)/$t/build PODS_CONFIGURATION_BUILD_DIR=$(pwd)/$t/build | xcpretty
+  set -o pipefail && xcodebuild -derivedDataPath $(pwd)/DerivedData -workspace JWBestPracticeApps.xcworkspace -scheme $t clean archive -configuration Release -archivePath $(pwd)/$t/build/$t.xcarchive | xcpretty
   set -o pipefail && xcodebuild -allowProvisioningUpdates -exportArchive -archivePath $(pwd)/$t/build/$t.xcarchive -exportOptionsPlist exportOptions.plist -exportPath $(pwd)/$t/build/Release-iphoneos | xcpretty
 done
