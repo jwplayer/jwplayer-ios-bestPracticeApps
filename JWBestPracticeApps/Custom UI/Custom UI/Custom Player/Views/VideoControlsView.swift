f09eeafb4364e8834f4b23ee6e3555e662754b82
Stephen Seibert <sseibert@jwplayer.com>
2022-02-08 15:58:09 -0500

    Added VideoControls

---
+++ JWBestPracticeApps/Custom UI/Custom UI/Custom Player/Views/VideoControlsView.swift
@@ -0,0 +1,29 @@
+//
+//  VideoControlsView.swift
+//  Custom UI
+//
+//  Created by Stephen Seibert on 2/8/22.
+//
+
+import Foundation
+import UIKit
+
+class VideoControlsView: XibView {
+    override var xibName: String { "VideoControls" }
+    
+    @IBOutlet weak var progressView: UIProgressView?
+    
+    override func setupView() {
+        super.setupView()
+        
+        progressView?.progress = 0.0
+    }
+    
+    @IBAction func onPlayPauseButtonTapped(_ button: UIButton) {
+        print("tapped")
+    }
+    
+    @IBAction func onFullScreenTapped(_ button: UIButton) {
+        print("tapped")
+    }
+}
