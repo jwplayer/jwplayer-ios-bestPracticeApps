101b91390804825e9cf8647a25823e2bf4618af6
Stephen Seibert <sseibert@jwplayer.com>
2022-02-08 14:13:56 -0500

    Designed and created AdControls interface

---
+++ JWBestPracticeApps/Custom UI/Custom UI/Custom Player/Views/AdControlsView.swift
@@ -0,0 +1,25 @@
+//
+//  AdControlsView.swift
+//  Custom UI
+//
+//  Created by Stephen Seibert on 2/8/22.
+//
+
+import Foundation
+import UIKit
+
+class AdControlsView: XibView {
+    override var xibName: String { "AdControls" }
+    
+    @IBAction func onPlayPauseButtonTapped(_ button: UIButton) {
+        
+    }
+    
+    @IBAction func onSkipTapped(_ button: UIButton) {
+        
+    }
+    
+    @IBAction func onLearnMoreTapped(_ button: UIButton) {
+        
+    }
+}
---
+++ JWBestPracticeApps/Custom UI/Custom UI/Custom Player/Views/XibView.swift
@@ -0,0 +1,53 @@
+//
+//  XibView.swift
+//  Custom UI
+//
+//  Created by Stephen Seibert on 2/8/22.
+//
+
+import Foundation
+import UIKit
+
+/**
+ All views which are defined by a XIB file use this superclass. This superclass should not be used on its own.
+ */
+class XibView: UIView {
+    @IBOutlet weak var contentView: UIView!
+    
+    open var xibName: String { "" }
+    
+    // MARK: - Lifecycle
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+
+    /**
+     Initial setup of the view.
+     */
+    open func setupView() {
+        guard load(xib: xibName, owner: self) != nil, contentView != nil else {
+            print("Failed to load XIB with name: '\(xibName).xib'")
+            return
+        }
+
+        addSubview(contentView)
+        contentView.fillSuperview()
+    }
+    
+    /**
+     Loads the xib the value represents.
+     - parameter owner: The object which will own the loaded xib.
+     - returns: An array of high level objects in the xib file.
+     */
+    private func load(xib name: String, owner: Any) -> [Any]? {
+        let objs = Bundle.main.loadNibNamed(name, owner: owner, options: nil)
+        return objs
+    }
+}
