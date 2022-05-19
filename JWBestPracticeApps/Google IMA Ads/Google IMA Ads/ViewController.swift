16e6df583dd5928ee7e101abccefd4234e0b3865
Amitai Blickstein <blickstein@gmail.com>
2022-05-18 21:39:57 -0400

    Ready for issue reproduction
    Tapping replay after (1) an ad has played, and (2) exiting PiP mode, crashes app with a `UIViewControllerHierarchyInconsistency`

--- JWBestPracticeApps/Google IMA Ads/Google IMA Ads/ViewController.swift
+++ JWBestPracticeApps/Google IMA Ads/Google IMA Ads/ViewController.swift
@@ -48,7 +48,7 @@ class ViewController: JWPlayerViewController {
                 .build()
 
             let adBreak = try JWAdBreakBuilder()
-                .offset(.midroll(percent: 50))
+                .offset(.preroll())
                 .tags([URL(string: "https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/single_preroll_skippable&sz=640x480&ciu_szs=300x250%2C728x90&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=")!])
                 .build()
             
