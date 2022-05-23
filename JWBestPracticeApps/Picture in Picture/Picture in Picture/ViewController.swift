cf7a18c4d638930ca042373507f2fe4127c22a17
David Almaguer <david.almaguer@enroutesystems.com>
2022-05-23 18:18:04 -0500

    Added base project for PiP

---
+++ JWBestPracticeApps/Picture in Picture/Picture in Picture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Picture in Picture
+//
+//  Created by David Almaguer on 23/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
