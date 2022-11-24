f02d8427bb312e18f640b0c8859fcea9bdb989d4
Amitai Blickstein <blickstein@gmail.com>
2022-11-23 22:42:44 -0500

    SERV-11482 - FairPlay DRM moved to Studio DRM

--- JWBestPracticeApps/DRM Fairplay/DRM Fairplay/ViewController.swift
+++ JWBestPracticeApps/DRM Fairplay/DRM Fairplay/ViewController.swift
@@ -8,55 +8,55 @@
 import UIKit
 import JWPlayerKit
 
-/// Appears to expire after a couple of minutes. Refresh using instructions in ticket.
-fileprivate let token = """
-    eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyZXNvdXJjZSI6Ii92Mi9tZWRpYS9reWJvN3g3Yi9kcm0vYWlVZWhJQVMiLCJleHAiOjE2Njg0ODk0Mzl9.XCXdXSESbZSA0Gq7lLifB8glsI2gZgAtIYWGM2zQKeA
-    """
-
-fileprivate var JWDRM_VideoEndpoint: String?
-fileprivate var JWDRM_CertificateEndpoint: String?
-fileprivate var JWDRM_SPCProcessEndpoint:  String?
-fileprivate var JWDRM_AppCertificateData: Data?
-
 class ViewController: JWPlayerViewController,
                       JWDRMContentKeyDataSource {
+    
+    /// All inputs required for the FairPlay DRM process (the "App" in figure 1-2 of [FairPlay Streaming Overview.pdf](https://developer.apple.com/streaming/fps/FairPlayStreamingOverview.pdf),
+    /// page 6) are prefetched and prepared ahead of time, and briefly stored in this struct for use in the content
+    /// key protocol's handlers.
+    var drmProperties: DRMContentKeyRequestInputs?
 
-    var contentUUID = "content-uuid"
-    let videoUrl = "https://cdn.jwplayer.com/v2/media/kybo7x7b/drm/aiUehIAS?token=\(token)"
-
+    /// Composed of the media ID, DRM policy ID, and a JWT generated from the two IDs + the property's client secret.
+    var signedVideoURL: URL {
+        guard var mutableUrl = URL(string: "https://cdn.jwplayer.com/v2/")
+        else { fatalError("App demo failed on: line \(#line)") }
+        
+        mutableUrl.append(path: "media/\(JWTestAsset.mediaId)")
+        mutableUrl.append(path: "drm/\(JWTestAsset.policyId)")
+        
+        // FIXME: This can and should be replaced with your preferred JWT generation method.
+        let token = TokenGenerator.createToken(
+            forMediaId: JWTestAsset.mediaId,
+            policyId: JWTestAsset.policyId,
+            expiration: 3600
+        )
+        
+        let tokenParam = URLQueryItem(name: "token", value: token)
+        mutableUrl.append(queryItems: [tokenParam])
+        
+        return mutableUrl
+    }
+    
+    
+    // MARK: Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Task is required to use await/async in viewDidLoad
         Task {
-            try await prefetchDRMData(protectedSource: videoUrl)
-            try await prefetchAppCertificate()
-            
-            guard prefetchedPropertiesDoExist()
-            else { fatalError("App demo failed on: line \(#line)") }
-            
-            self.setUpPlayer()
+            drmProperties = try await prefetchDRMData(protectedSource: signedVideoURL)
+            guard let drmProperties else { fatalError("App demo failed on: line \(#line)") }
+            self.setUpPlayer(withDRMProtectedURL: drmProperties.videoEndpoint)
         }
-    }
-    
-    /// Checks to make sure the async operations populated the values successfully.
-    private func prefetchedPropertiesDoExist() -> Bool {
-        ([
-            JWDRM_VideoEndpoint,
-            JWDRM_CertificateEndpoint,
-            JWDRM_SPCProcessEndpoint,
-            JWDRM_AppCertificateData,
-        ] as [Any?])
-            .allSatisfy({ $0 != nil })
     }
     
     /**
      Sets up the player with a DRM configuration.
      */
-    private func setUpPlayer() {
+    private func setUpPlayer(withDRMProtectedURL videoUrl: URL) {
         // Open a do-catch block to catch possible errors with the builders.
         do {
-            let videoUrl = URL(string: JWDRM_VideoEndpoint!)!
-
             // First, use the JWPlayerItemBuilder to create a JWPlayerItem that will be used by the player configuration.
             let playerItem = try JWPlayerItemBuilder()
                 .file(videoUrl)
@@ -64,7 +64,7 @@ class ViewController: JWPlayerViewController,
 
             // Second, create a player config with the created JWPlayerItem.
             let config = try JWPlayerConfigurationBuilder()
-                .playlist([playerItem])
+                .playlist(items: [playerItem])
                 .autostart(true)
                 .build()
 
@@ -80,33 +80,27 @@ class ViewController: JWPlayerViewController,
         }
     }
 
+    
     // MARK: JWDRMContentKeyDataSource
 
     /**
      When called, this delegate method requests the identifier for the protected content to be passed through the delegate method's completion block.
      */
     func contentIdentifierForURL(_ url: URL, completionHandler handler: @escaping (Data?) -> Void) {
-        print(#function)
-
-        let uuidData = contentUUID.data(using: .utf8)
+        let uuidData = DRMContentKeyRequestInputs.contentUUID.data(using: .utf8)
         handler(uuidData)
     }
-
 
     /**
      When called, this delegate method requests an Application Certificate binary which must be passed through the completion block.
      - note: The Application Certificate must be encoded with the X.509 standard with distinguished encoding rules (DER). It is obtained when registering an FPS playback app with Apple, by supplying an X.509 Certificate Signing Request linked to your private key.
      */
     func appIdentifierForURL(_ url: URL, completionHandler handler: @escaping (Data?) -> Void) {
-        print(#function)
-        print("url: \(url)")
-        
-        guard let appIdData = JWDRM_AppCertificateData
-        else {
+        guard let drmProperties else {
             handler(nil)
             return
         }
-        handler(appIdData)
+        handler(drmProperties.appCertificateData)
     }
 
     /**
@@ -117,86 +111,99 @@ class ViewController: JWPlayerViewController,
      - note: For resources that may expire, specify a renewal date and the content-type in the completion block.
      */
     func contentKeyWithSPCData(_ spcData: Data, completionHandler handler: @escaping (Data?, Date?, String?) -> Void) {
-        print(#function)
-
-        guard
-            let spcEndpoint = JWDRM_SPCProcessEndpoint,
-            let licenseApiPath = URL(string: spcEndpoint)
+        guard let drmProperties
         else { fatalError("App demo failed on: line \(#line)") }
-        
-        var ckcRequest = URLRequest(url: licenseApiPath)
+
+        var ckcRequest        = URLRequest(url: drmProperties.spcProcessEndpoint)
         ckcRequest.httpMethod = "POST"
-        ckcRequest.httpBody = spcData
+        ckcRequest.httpBody   = spcData
         ckcRequest.addValue("application/octet-stream", forHTTPHeaderField: "Content-Type")
 
         Task {
             let (data, response) = try await URLSession.shared.data(for: ckcRequest)
-            guard (200...299).contains((response as? HTTPURLResponse)!.statusCode) else {
+          
+            // handle error response
+            guard response.httpStatusCode == 200
+            else {
                 handler(nil, nil, nil)
                 throw DemoError.statusCode(line: #line)
             }
 
+            // happy path
             handler(data, nil, nil)
         }
     }
 }
 
 // MARK: Helpers
+// Keeping the code clean and organized.
 
 extension ViewController {
-    /// Get all the endpoints generated and set, so the request can run smoothly.
-    func prefetchDRMData(protectedSource: String) async throws {
-        print(#function)
-        
-        // 1. URLRequest composition
-        guard let sourceURL = URL(string: protectedSource)
-        else { fatalError("App demo failed on: line \(#line)") }
-        
-        var request = URLRequest(url: sourceURL,
-                                 cachePolicy: .useProtocolCachePolicy,
-                                 timeoutInterval: 10)
-        request.allHTTPHeaderFields = ["Accept": "application/json; charset=utf-8"]
-        
-        print("About to fetch with request: \n\(request.description)")
-        
-        // 2. URLSession fetch and response handling
-        let (data, response) = try await URLSession.shared.data(for: request)
-        guard (response as? HTTPURLResponse)?.statusCode == 200 else {
-            throw DemoError.statusCode(line: #line)
-        }
-        let jsonData = try newJSONDecoder().decode(DeliveryAPI.self, from: data)
-        
-        // 3a. Get the index of a source with a "fairplay" element.
-        guard let fairPlayIndex = jsonData.playlist[0].sources
-            .firstIndex(where: { $0.drm.fairplay != nil })
-        else { fatalError("App demo failed on: line \(#line)") }
-        
-        // 3b. Assign relevant fields from fetched DeliveryAPI for our stream config
-        let drmItem = jsonData.playlist[0].sources[fairPlayIndex]
-        JWDRM_CertificateEndpoint = drmItem.drm.fairplay?.certificateURL
-        JWDRM_VideoEndpoint       = drmItem.file
-        JWDRM_SPCProcessEndpoint  = drmItem.drm.fairplay?.processSpcURL
-        
-        print("DRM-Log certificate -  \(JWDRM_CertificateEndpoint!)")
-        print("DRM-Log video -  \(JWDRM_VideoEndpoint!)")
-        print("DRM-Log spc_process -  \(JWDRM_SPCProcessEndpoint!)")
-    }
-    
-    func prefetchAppCertificate() async throws {
-        guard
-            let certEndpoint = JWDRM_CertificateEndpoint,
-            let certUrl = URL(string: certEndpoint)
-        else { fatalError("App demo failed on: line \(#line)") }
-        
-        let (data, response) = try await URLSession.shared.data(from: certUrl)
-        guard (response as? HTTPURLResponse)?.statusCode == 200 else {
-            throw DemoError.statusCode(line: #line)
-        }
+    /// Get all the endpoints generated and store as state (briefly) so the intent of the drm callback
+    /// methods is self-documenting.
+    func prefetchDRMData(protectedSource sourceURL: URL) async throws -> DRMContentKeyRequestInputs {
+        // 1. URLSession fetch, and response handling
+        let (data, response) = try await URLSession.shared.data(from: sourceURL)
 
-        JWDRM_AppCertificateData = data
+        guard response.httpStatusCode == 200
+        else { throw DemoError.statusCode(line: #line) }
+        
+        let jsonResponse = try newJSONDecoder().decode(DeliveryAPI.self, from: data)
+        
+        guard
+            // 2. Find the drm object mapped from the response...
+            let drmItem = jsonResponse.playlist[0].sources.first(where: { $0.drm.fairplay != nil }),
+            // 3. ...then unpack the certificate and spc urls.
+            let certUrl = drmItem.drm.fairplay?.certificateURL.toURL(),
+            let spcUrl  = drmItem.drm.fairplay?.processSpcURL
+        else { fatalError("App demo failed on: line \(#line)") }
+                
+        let (certData, certResponse) = try await URLSession.shared.data(from: certUrl)
+        
+        guard certResponse.httpStatusCode == 200
+        else { throw DemoError.statusCode(line: #line) }
+
+        // 4. Finally, with all the required endpoints/data in hand, return the object to hold
+        // them all for later retrieval by the DRM delegate methods.
+        return DRMContentKeyRequestInputs(
+            videoEndpoint: drmItem.file,
+            spcProcessEndpoint: spcUrl,
+            appCertificateData: certData
+        )
     }
 }
 
+/// For non-fatal errors.
 enum DemoError: Error {
     case statusCode(line: Int)
+}
+
+/// Initializes with the inputs the developer/customer has at hand for easy usage in the DRM delegate methods.
+struct DRMContentKeyRequestInputs {
+    let videoEndpoint: URL
+    let spcProcessEndpoint:  URL
+    let appCertificateData: Data
+    static let contentUUID = "content-uuid"
+    
+    init(videoEndpoint ve: String, spcProcessEndpoint spe: String, appCertificateData: Data) {
+        guard
+            let videoEndpoint      = ve.toURL(),
+            let spcProcessEndpoint = spe.toURL()
+        else { fatalError("App demo failed on: line \(#line)") }
+        
+        self.videoEndpoint      = videoEndpoint
+        self.spcProcessEndpoint = spcProcessEndpoint
+        self.appCertificateData = appCertificateData
+    }
+}
+
+// MARK: Sugar
+extension String {
+    func toURL() -> URL? { URL(string: self) }
+}
+
+extension URLResponse {
+    private var asHTTPURLResponse: HTTPURLResponse? { self as? HTTPURLResponse }
+    /// - returns: The status code if applicable, otherwise `-1`.
+    var httpStatusCode: Int { (asHTTPURLResponse?.statusCode) ?? -1 }
 }
