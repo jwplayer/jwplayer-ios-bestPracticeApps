777e631ba4ca63069bfed116f8c3495a29e97a57
David Almaguer <c-dalmaguer@jwplayer.com>
2023-02-17 09:50:53 -0600

    Standardize setLicenseKey usage

--- JWBestPracticeApps/Google IMA companion ads/Google IMA Companion Ads/AppDelegate.swift
+++ JWBestPracticeApps/Google IMA companion ads/Google IMA Companion Ads/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         // Add your JW Player license key.
-        JWPlayerKitLicense.setLicenseKey("")
+        // JWPlayerKitLicense.setLicenseKey(<#key: String#>)
         
         return true
     }
