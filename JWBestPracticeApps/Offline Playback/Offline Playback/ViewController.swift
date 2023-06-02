b65b7506196983ff9157ad319bd3df70ca6772cc
Stephen Seibert <sseibert@jwplayer.com>
2023-06-02 10:33:48 -0400

    Created demo project for Offline Playback

---
+++ JWBestPracticeApps/Offline Playback/Offline Playback/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Offline Playback
+//
+//  Created by Stephen Seibert on 6/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
